17a4fb4b8299e8c2bb65eb8101856fb387a15165
justjoshriley <joshriley316@gmail.com>
2021-03-23 13:16:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    // anchors: ['section1', 'section2', 'section3', 'section4'],
+    navigationTooltips: ['Home', 'About', 'Projects', 'Contact']
+})
